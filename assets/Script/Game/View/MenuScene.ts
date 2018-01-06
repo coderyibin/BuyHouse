f810687d2ed14ec2f269d9210dc9a4adf53da53b
linyibin <545115382@qq.com>
2018-01-06 17:23:06 +0800

    添加资源

---
+++ assets/Script/Game/View/MenuScene.ts
@@ -0,0 +1,8 @@
+import SceneComponent from "../../Frame/view/SceneComponent";
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class MenuScene extends SceneComponent {
+    
+}
